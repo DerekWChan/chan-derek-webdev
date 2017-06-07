f514fcc6c0c13e2cf29ab647efae660965674538
Derek Chan <derekchan7@gmail.com>
2017-06-06 23:38:56 -0400

    Implement WbdvSortable directive

---
+++ public/assignment/directives/wbdv-sortable.js
@@ -0,0 +1,39 @@
+(function() {
+  angular
+    .module('WebAppMaker')
+    .directive('WbdvSortable', WbdvSortable);
+
+    function WbdvSortable() {
+      function linkFunction(scope, element) {
+        var initialIndex = -1;
+        var finalIndex = -1;
+        element.sortable({
+          start: function(event, ui) {
+            initialIndex = $(ui.item).index();
+          },
+          stop: function(event, ui) {
+            finalIndex = $(ui.item).index();
+            scope.SortWidgetController.sort(initialIndex, finalIndex);
+          },
+          axis: 'y'
+        });
+      }
+
+      return {
+        scope: {},
+        link: linkFunction,
+        controller: SortWidgetController,
+        controllerAs: 'SortWidgetController'
+      }
+    }
+
+    function SortWidgetController(WidgetService, $routeParams) {
+      var model = this;
+      model.pageId = $routeParams.pageId;
+      model.sort = sort;
+
+      function sort(initialIndex, finalIndex) {
+        WidgetService.sortWidget(initial, final, model.pageId);
+      }
+    }
+  })();
