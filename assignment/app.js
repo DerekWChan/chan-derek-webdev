49da704b5935b947c5275eb0649828e313e2ae85
Derek Chan <derekchan7@gmail.com>
2017-06-12 23:55:13 -0400

    Add and configure new dependencies

--- assignment/app.js
+++ assignment/app.js
@@ -1,3 +1,16 @@
+var mongoose = require('mongoose');
+var connectionString = 'mongodb://localhost/webdev';
+mongoose.Promise = require('q').Promise;
+
+if(process.env.MLAB_USERNAME_WEBDEV) {
+    var username = process.env.MLAB_USERNAME_WEBDEV;
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds137291.mlab.com:37291/heroku_tlmqrq0t';
+}
+
+mongoose.connect(connectionString);
+
 require('./services/user.service.server');
 require('./services/website.service.server');
 require('./services/page.service.server');
