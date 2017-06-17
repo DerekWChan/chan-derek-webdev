ce57e8f1b68a6f0eaa8b8d6c6a085bba3a084908
Derek Chan <derekchan7@gmail.com>
2017-06-17 16:32:38 -0400

    Configure Express Session Support

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var app = require('./express');
 var bodyParser = require('body-parser');
+var passport     = require('passport');
+var cookieParser = require('cookie-parser');
+var session      = require('express-session');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+app.use(session({ secret: process.env.SESSION_SECRET }));
 
 // configure a public directory to host static content
 app.use(app.express.static(__dirname + '/public'));
